982c844002cb40c8d5e035b0a797cb617c96ab26
eggs <550467116@.qq.com>
2021-04-07 16:13:53 +0800

    创建Layout组件

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+    requireContext.keys().forEach(requireContext);
+try {
+    importAll(require.context('icons', true, /\.svg$/));
+} catch (error) {
+    console.log(error);
+}
 
 type Props = {
-    name:string
+    name: string
 }
 
-const Icon = (props: Props)=>{
-    return(
+const Icon = (props: Props) => {
+    return (
         <svg className="icon">
-            <use xlinkHref={'#'+props.name}/>
+            <use xlinkHref={'#' + props.name}/>
         </svg>
-    )
-}
+    );
+};
 
 export default Icon;
