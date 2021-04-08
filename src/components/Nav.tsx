726f4b3fde677f213350dc6d3ee8dd7325b4fc13
eggs <550467116@.qq.com>
2021-04-08 11:24:57 +0800

    修复Nav点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,48 +1,57 @@
-import styled from "styled-components";
-import {Link} from "react-router-dom";
-import React from "react";
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
 import Icon from './Icons';
 
 
 const NavWrapper = styled.nav`
-box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
->ul{
-   display: flex;
-   >li{
-   width: 33.333%;
-   text-align: center;
-   padding:4px 0;
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   .icon{
-   width:24px;
-   height: 24px;
-   }
-   }
-}
+
+  > ul {
+    display: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      > a {
+        padding: 4px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+      }
+    }
+  }
 `;
 
-const Nav = ()=>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="tag"/>
-                    <Link to="/tags">标签页</Link>
+                    <Link to="/tags">
+                        <Icon name="tag"/>
+                        标签页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="money"/>
-                    <Link to="/money">记账页</Link>
+                    <Link to="/money">
+                        <Icon name="money"/>
+                        记账页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="chart"/>
-                    <Link to="/statistics">统计页</Link>
+                    <Link to="/statistics">
+                        <Icon name="chart"/>
+                        统计页
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
-export default Nav
+    );
+};
+export default Nav;
