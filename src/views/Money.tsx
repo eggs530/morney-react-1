7c189c86f1441ade0063cfdf4797427dd0f269e8
eggs <550467116.com>
2021-04-11 20:54:15 +0800

    重构优化表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -20,39 +20,31 @@ function Money() {
         category: "-" as Category,
         amount: 0
     });
+    const onChange = (obj: Partial<typeof selected>) => {//obj是selected的一部分
+        setSelected({
+            ...selected,
+            ...obj
+        });
+    };
     return (
         <MyLayout>
             {selected.tags.join(",")}
             <hr/>
             {selected.note}
+            <hr/>
             {selected.category}
+            <hr/>
             {selected.amount}
             <TagsSection value={selected.tags}
-                         onChange={(tags) => setSelected({
-                             ...selected,
-                             tags: tags
-                         })}/>
+                         onChange={tags => onChange({tags})}/>
             <NoteSection value={selected.note}
-                         onChange={(note) => setSelected({
-                             ...selected,
-                             note: note
-                         })}
-            />
+                         onChange={note => onChange({note})}/>
             <CategorySection value={selected.category}
-                             onChange={(category) => {
-                                 setSelected({
-                                     ...selected,
-                                     category: category
-                                 })
-                             }}
-            />
+                             onChange={category => onChange({category})}/>
             <NumberPadSection value={selected.amount}
-                              onChange={(amount) => setSelected({
-                                  ...selected,
-                                  amount: amount
-                              })}
-                              onOK={()=>{}}
-            />
+                              onChange={amount => onChange({amount})}
+                              onOK={() => {
+                              }}/>
         </MyLayout>
     );
 }
