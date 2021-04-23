3ba3958209c04e0d109dd227f444eade30276591
eggs <550467116@.qq.com>
2021-04-23 11:54:02 +0800

    重构createId让id自增，并使用defaultTags防止多次初始化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,17 +1,20 @@
 import {useState} from 'react';
+import createId from './lb/createId';
 
-const useTags = ()=>{
-    const [tags, setTags] = useState<{id:number;name:string}[]>
-    ([
-        {id:1,name:'衣'},
-        {id:2,name:'食'},
-        {id:3,name:'住'},
-        {id:4,name:'行'}
-    ]);
-    return{
-        tags:tags,
-        setTags:setTags
-    }
-}
 
-export default useTags
+const defaultTags = [
+    {id: createId(), name: '衣'},
+    {id: createId(), name: '食'},
+    {id: createId(), name: '住'},
+    {id: createId(), name: '行'}
+];
+const useTags = () => {
+    const [tags, setTags] = useState<{ id: number; name: string }[]>
+    (defaultTags);
+    return {
+        tags: tags,
+        setTags: setTags
+    };
+};
+
+export default useTags;
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import useTags from 'useTags';
+import createId from '../../lb/createId';
 
 const Wrapper = styled.section`
   background: #FFFFFF;
@@ -47,7 +48,7 @@ const TagSection: React.FunctionComponent<Props> = (props) => {
     const onAddTag = () => {
         const tagName = window.prompt("新增的标签名为");
         if (tagName !== null) {
-            setTags([...tags, {id:Math.random(),name:tagName}]);
+            setTags([...tags, {id:createId(),name:tagName}]);
         }
     };
     const onToggleTag = (tagId: number) => {
