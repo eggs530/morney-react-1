0107667514d0176f7421cef6089bb6b31d2cc8e3
eggs <550467116@.qq.com>
2021-04-23 17:22:32 +0800

    创建单独的Input组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+    display: flex;
+    align-items: center;
+
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      border: none;
+      background: none;
+    }
+`
+type Props = {
+    label:string;
+}&React.InputHTMLAttributes<HTMLInputElement>
+const Input:React.FC<Props>=(props)=>{
+    const {label,children,...rest}=props;//把除了label和children的所有属性都拷贝过去
+    return(
+    <Label>
+        <span>{props.label}</span>
+        <input{...rest}/>
+    </Label>
+    )
+}
+
+
+export default Input
