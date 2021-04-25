e3dd041e937a9a9d0397ffe78b01547dac68373c
eggs <550467116@.qq.com>
2021-04-25 17:04:55 +0800

    Tag.tsx的样式完成

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,6 +5,9 @@ import Layout from 'components/Layout';
 import Icon from '../components/Icons';
 import Button from '../components/Button';
 import styled from 'styled-components';
+import Input from '../components/Input';
+import Center from '../components/Center';
+import Space from '../components/Space';
 
 type Params = {
     id: string
@@ -13,10 +16,15 @@ const Topbar = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  line-height:14px;
-  padding:14px;
-  background:white;
+  line-height: 14px;
+  padding: 14px;
+  background: white;
 `;
+const InputWrapper = styled.div`
+    background: white;
+  padding:14px 16px;
+  font-size: 14px;
+`
 const Tag: React.FC = () => {
     const {findTag} = useTags();
     let {id} = useParams<Params>();
@@ -28,15 +36,14 @@ const Tag: React.FC = () => {
                 <span>编辑标签</span>
                 <Icon/>
             </Topbar>
-            <div>
-                <label>
-                    <span>标签名</span>
-                    <input type="text" placeholder="标签名"/>
-                </label>
-                <div>
-                    <Button>删除标签</Button>
-                </div>
-            </div>
+            <InputWrapper>
+                <Input label="标签名" type="text" placeholder="请填写备注" value="tag.name"/>
+            </InputWrapper>
+            <Space/>
+            <Space/>
+            <Center>
+                <Button>删除标签</Button>
+            </Center>
         </Layout>
     );
 };
