167e55821a6e5df105641a7ee5ffb501eeed8063
eggs <550467116@.qq.com>
2021-04-27 11:59:19 +0800

    简化标签的更新和删除

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,31 +10,23 @@ const defaultTags = [
 ];
 const useTags = () => {
     const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-    const findTag = (id:number) =>tags.filter(tag=>tag.id===id)[0]
-    const findTagIndex = (id:number)=>{
-        let result = -1//避免id不存在于这个数组而返回i
-        for(let i =0 ;i<tags.length;i++){
-            if(tags[i].id===id){
-                result = i
-                break
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+        let result = -1;//避免id不存在于这个数组而返回i
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
+                result = i;
+                break;
             }
         }
-        return result
-    }
-    const updateTag = (id:number,obj:{name:string}) =>{
-        const index = findTagIndex(id)
-        //深拷贝 tags 得到 tasClone
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        //把tagsClone 的第index删掉，换成新的{id:id,name:obj.name}
-        tagsClone.splice(index,1,{id:id,name:obj.name})
-        setTags(tagsClone)
-    }
-    const deleteTag = (id:number)=>{
-        const index = findTagIndex(id)
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        tagsClone.splice(index,1)
-        setTags(tagsClone)
-    }
+        return result;
+    };
+    const updateTag = (id: number, obj: { name: string }) => {
+        setTags(tags.map(tag => tag.id === id ? {id: id, name: obj.name} : tag));
+    };
+    const deleteTag = (id: number) => {
+        setTags(tags.filter(tag => tag.id !== id));
+    };
     return {
         tags,
         setTags,
