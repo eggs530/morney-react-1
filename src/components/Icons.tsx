c3404e20f52fa26e1306817e9313f8be896a9467
eggs <550467116@.qq.com>
2021-04-27 16:00:57 +0800

    点击回退

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames'
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
     requireContext.keys().forEach(requireContext);
@@ -10,12 +11,13 @@ try {
 
 type Props = {
     name?: string
-}
+}& React.SVGAttributes<SVGElement> //使其接受SVG的所有属性
 
 const Icon = (props: Props) => {
+    const {name,children,className,...rest} = props
     return (
-        <svg className="icon">
-            {props.name&& <use xlinkHref={'#' + props.name}/>}
+        <svg className={classnames('icon',className)} {...rest}>
+            {props.name && <use xlinkHref={'#' + props.name}/>}
         </svg>
     );
 };
