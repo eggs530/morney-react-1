ba401438824a79686b9928783979d92986ad84eb
eggs <550467116@.qq.com>
2021-04-29 16:55:21 +0800

    解决无法输入点的bug

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Wrapper from "./NumberPadSection/Wrapper";
-import generateOutput from "./NumberPadSection/generateOutput";
+import React, {useState} from 'react';
+import Wrapper from './NumberPadSection/Wrapper';
+import generateOutput from './NumberPadSection/generateOutput';
 
 type Props = {
     value: number;
@@ -8,30 +8,32 @@ type Props = {
     onOK?: () => void;
 }
 const NumberPadSection: React.FC<Props> = (props) => {
-    const output = props.value.toString();
+    //const output = props.value.toString();
+    const [output, _setOutput] = useState(props.value.toString());
     const setOutput = (output: string) => {
-        let value;
+        let newOutput: string;
         if (output.length > 16) {
-            value = parseFloat(output.slice(0, 16));
+            newOutput = output.slice(0, 16);
         } else if (output.length === 0) {
-            value = 0;
+            newOutput = '0';
         } else {
-            value = parseFloat(output);
+            newOutput = output;
         }
-        props.onChange(value);
+        _setOutput(newOutput);
+        props.onChange(parseFloat(newOutput));
     };
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent;
         if (text === null) {
             return;
         }
-        if (text === "OK") {
+        if (text === 'OK') {
             if (props.onOK) {
                 props.onOK();
             }
             return;
         }
-        if ("0123456789.".split("").concat(["清空", "删除"]).indexOf(text) >= 0) {
+        if ('0123456789.'.split('').concat(['清空', '删除']).indexOf(text) >= 0) {
             setOutput(generateOutput(text, output));
         }
     };
