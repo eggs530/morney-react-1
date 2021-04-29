3e58c38906dc9885da2d3eb1b1d5f543d1f68adf
eggs <550467116@.qq.com>
2021-04-29 16:37:11 +0800

    检查输入

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,39 +1,40 @@
-import Layout from "../components/Layout";
-import React, {useState} from "react";
-import styled from "styled-components";
-import TagsSection from "./Money/TagsSection";
-import NoteSection from "./Money/NoteSection";
-import CategorySection from "./Money/CategorySection";
-import NumberPadSection from "./Money/NumberPadSection";
+import Layout from '../components/Layout';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import TagsSection from './Money/TagsSection';
+import NoteSection from './Money/NoteSection';
+import CategorySection from './Money/CategorySection';
+import NumberPadSection from './Money/NumberPadSection';
 import {useRecords} from '../hooks/useRecords';
 
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
-type Category = "-" | "+"
+type Category = '-' | '+'
 
 const defaultFormData = {
     tagIds: [] as number[],
-    note: "",
-    category: "-" as Category,
+    note: '',
+    category: '-' as Category,
     amount: 0
-}
+};
 
 function Money() {
     const [selected, setSelected] = useState(defaultFormData);
-    const {records,addRecord}=useRecords()
+    const {records, addRecord} = useRecords();
     const onChange = (obj: Partial<typeof selected>) => {//obj是selected的一部分
         setSelected({
             ...selected,
             ...obj
         });
     };
-    const submit = ()=>{
-        addRecord(selected)
-        alert('保存成功')
-        setSelected(defaultFormData)
-    }
+    const submit = () => {
+        if (addRecord(selected)) {
+            alert('保存成功');
+            setSelected(defaultFormData);
+        }
+    };
     return (
         <MyLayout>
             <TagsSection value={selected.tagIds}
@@ -45,7 +46,7 @@ function Money() {
             <NumberPadSection value={selected.amount}
                               onChange={amount => onChange({amount})}
                               onOK={submit}
-                                  />
+            />
         </MyLayout>
     );
 }
