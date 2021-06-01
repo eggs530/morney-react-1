b03b1366f5f7552fff08236031b2e244e2a060a3
eggs <550467116@.qq.com>
2021-06-01 10:39:52 +0800

    update title

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m "deploy" &&
-git remote add origin git@github.com:eggs530/test530.git &&
+git remote add origin git@github.com:eggs530/morney-react--website.git &&
 git branch -M main &&
 git push -u origin main -f &&
-cd-
+cd -
